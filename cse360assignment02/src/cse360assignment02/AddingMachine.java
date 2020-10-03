7acd6aaba6164540a0caabed3f25def966bb3dc8
joie <joie@localhost>
2020-10-03 17:23:07 -0600

    First commit.

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,39 @@
+/*
+ I'm not sure what I am supposed to comment...
+ */
+package cse360assignment02;
+
+public class AddingMachine 
+{
+	private int total;
+  
+	public AddingMachine () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+  
+	public int getTotal () 
+	{
+		return 0;
+	}
+  
+	public void add (int value) 
+	{
+		
+	}
+
+	public void subtract (int value) 
+	{
+		
+	}
+
+	public String toString () 
+	{
+		return "";
+	}
+
+	public void clear() 
+	{
+		
+	}
+}
